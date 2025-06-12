dd6418a7c40759679ca9646ed7628d9ab6ed0f0d
Nyoni Safaris <39374037+Monkeygene@users.noreply.github.com>
2025-06-12 08:51:28 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./docs/**/*.html",
+    "./docs/**/*.js",
+    "./src/**/*.css",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        avenir: ['Avenir', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
